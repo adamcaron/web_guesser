81c5543bb8835194f3435d221333362bd7291821
Adam Caron <adamwcaron@gmail.com>
2015-06-30 19:05:31 -0600

    changes background color like a boss based on the user's guess

--- web_guesser.rb
+++ web_guesser.rb
@@ -2,33 +2,46 @@ require 'sinatra'
 require 'sinatra/reloader'
 
 SECRET_NUMBER = Random.rand(3)
-bg_color      = "white"
 
 def check_guess(guess)
   if guess.to_i > SECRET_NUMBER
-    if (guess.to_i - SECRET_NUMBER) > 5
-      bg_color = "red"
+    if (guess.to_i - S
       message  = "Way too high!"
     else
-      bg_color = "#FFAAAA"
       message  = "Too high!"
+    end
+  elsif guess.to_i < SECRET_NUMBER
+    if (SECRET_NUMBER
+      message  = "Way too low!"
+    else
+      message  = "Too low!"
+    end
+  else
+    message    = "You got it right!" + "<p>The SECRET NUMBER is #{SECRET_NUMBER}</p>"
+  end
+end
+
+def bg_color(guess)
+  if guess.to_i > SECRET_NUMBER
+    if (guess.to_i - SECRET_NUMBER) > 5
+      bg_color = "red"
+    else
+      bg_color = "#FFAAAA"
     end
   elsif guess.to_i < SECRET_NUMBER
     if (SECRET_NUMBER - guess.to_i) > 5
       bg_color = "red"
-      message  = "Way too low!"
     else
       bg_color = "#FFAAAA"
-      message  = "Too low!"
     end
   else
-    bg_color = "#AAFFAA"
-    message  = "You got it right!" + "<p>The SECRET NUMBER is #{SECRET_NUMBER}</p>"
+    bg_color   = "#AAFFAA"
   end
 end
 
 get '/' do
-  guess   = params["guess"]
-  message = check_guess(guess)
+  guess    = params["guess"]
+  message  = check_guess(guess)
+  bg_color = bg_color(guess)
   erb :index, :locals => {:number => SECRET_NUMBER, :message => message, :bg_color => bg_color}
 end
